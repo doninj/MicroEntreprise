870a1006db3dfaefd13afffcb85a6699480caf26
doninj <jean-baptiste.38@hotmail.com>
2021-05-03 17:56:54 +0200

    ajout des seeders et des factory pour le reste

--- MicroEntreprise/database/seeders/DatabaseSeeder.php
+++ MicroEntreprise/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Mission;
+use App\Models\Organisation;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        //\App\Models\Organisation::factory(10)->create();
+        //\App\Models\Mission::factory(10)->create();
+        //\App\Models\MissionLine::factory(5)->create();
     }
 }
