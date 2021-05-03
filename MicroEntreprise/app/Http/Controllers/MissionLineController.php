e5ea799babb2b62110d23345d74cc65b43ec4a88
doninj <jean-baptiste.38@hotmail.com>
2021-05-03 15:32:19 +0200

    ajout de mission line

---
+++ MicroEntreprise/app/Http/Controllers/MissionLineController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MissionLine;
+use Illuminate\Http\Request;
+
+class MissionLineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MissionLine  $missionLine
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MissionLine $missionLine)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MissionLine  $missionLine
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MissionLine $missionLine)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MissionLine  $missionLine
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MissionLine $missionLine)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MissionLine  $missionLine
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MissionLine $missionLine)
+    {
+        //
+    }
+}
---
+++ MicroEntreprise/database/seeders/MissionLineSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class MissionLineSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
