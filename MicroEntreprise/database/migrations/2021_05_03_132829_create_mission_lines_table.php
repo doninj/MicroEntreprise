61975d4c556dd1c04fbac99bd10fecdfaee7ba52
doninj <jean-baptiste.38@hotmail.com>
2021-05-03 15:32:10 +0200

    ajout de mission line

---
+++ MicroEntreprise/database/migrations/2021_05_03_132829_create_mission_lines_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMissionLinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mission_lines', function (Blueprint $table) {
+          $table->uuid('id')->primary();
+          $table->uuid('mission_id');
+          $table->string('title');
+          $table->integer('quantity');
+          $table->integer('price');
+          $table->string('unity');
+          $table->timestamps();
+
+          $table->foreign('mission_id')->refences('id')->on('missions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mission_lines');
+    }
+}
